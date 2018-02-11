ef6162160ed7cbebfd02306273c3fa1d7692b1a3
psswid <psswiderski@gmail.com>
2018-02-11 23:36:21 +0100

    validation

--- app/container.php
+++ app/container.php
@@ -9,9 +9,12 @@ use Interop\Container\ContainerInterface;
 use Slim\Interfaces\RouterInterface;
 use Cart\Support\Storage\SessionStorage;
 use Cart\Support\Storage\Contracts\StorageInterface;
+use Cart\Validation\Contracts\ValidatorInterface;
+use Cart\Validation\Validator;
 
 return [
   //'router'  => get(Slim\Router::class), <= as that, can't recognize router
+  ValidatorInterface::class => function (ContainerInterface $c) {return new Validator;},
   RouterInterface::class => function (ContainerInterface $c) { return $c->get('router'); },
   StorageInterface::class => function (ContainerInterface $c) { return new SessionStorage('cart'); },
   Twig::class => function(ContainerInterface $c){
--- app/routes.php
+++ app/routes.php
@@ -9,3 +9,7 @@ $app->get('/cart', ['Cart\Controllers\CartController', 'index'])->setName('cart.
 $app->get('/cart/add/{slug}/{quantity}', ['Cart\Controllers\CartController', 'add'])->setName('cart.add');
 
 $app->post('/cart/update/{slug}', ['Cart\Controllers\CartController', 'update'])->setName('cart.update');
+
+$app->get('/order', ['Cart\Controllers\OrderController', 'index'])->setName('order.index');
+
+$app->post('/order', ['Cart\Controllers\OrderController', 'create'])->setName('order.create');
